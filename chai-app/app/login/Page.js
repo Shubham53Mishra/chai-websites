774884545236d8a3c5e4ddb012b9f211b7ed8173
Shubham mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2024-06-10 21:19:03 +0530

    master branch

--- chai-app/app/login/Page.js
+++ chai-app/app/login/Page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
     return (
         <div className='text-white'>
-            <h1 className='font-bold'>Login to Get Your fans to like to simple Support You</h1>
+            <h1 className='font-bold'>Login to Get Your fans to  Support You</h1>
         </div>
     )
 }
