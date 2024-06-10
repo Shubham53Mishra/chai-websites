d98c89b0b889b091a3e5c8d47a460bae9e0ca2ac
Shubham mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2024-06-10 21:25:14 +0530

    sorry to commit

--- chai-app/app/login/Page.js
+++ chai-app/app/login/Page.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Login = () => {
     return (
         <div className='text-white'>
+<<<<<<< HEAD
             <h1 className='font-bold'>Login to Get Your fans to  Support You</h1>
+=======
+            <h1 className='font-bold'>Login to Get Your fans to super to come simple Support You</h1>
+>>>>>>> master
         </div>
     )
 }
