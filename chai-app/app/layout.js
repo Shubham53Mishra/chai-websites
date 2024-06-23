bad73d4dc9cca7aa284094efff988daf777669f9
Shubham mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2024-06-23 16:36:16 +0530

    change login

--- chai-app/app/layout.js
+++ chai-app/app/layout.js
@@ -15,11 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="min-h-[83vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-          {children}
+        <SessionWrapper>
+          <Navbar />
+          <div className="min-h-[83vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+            {children}
           </div>
-        <Footer />
+          <Footer />
+        </SessionWrapper>
       </body>
     </html>
   );
--- chai-app/components/Navbar.js
+++ chai-app/components/Navbar.js
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client"
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link';
 
 const Navbar = () => {
